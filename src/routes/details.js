be32d26a5e8210b340de0cc1a0c03de5dcca412d
Donovan <donovanjuniogomez@gmail.com>
2022-01-16 01:33:50 -0800

    added npm package called Marked to covert markdown file in dangerouslySetInnerHTML because API returns markdown for about page

--- src/routes/details.js
+++ src/routes/details.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { findByLabelText } from "@testing-library/react";
+import { marked } from "marked";
 
 const Section = styled.section`
   min-height: 100vh;
@@ -33,8 +33,16 @@ const Name = styled.h1`
   font-size: 5rem;
 `;
 
+const JpName = styled.h2`
+  font-size: 4rem;
+`;
+
 const NameWrapper = styled.div`
   flex: 1;
+`;
+
+const About = styled.div`
+  font-size: 1.15rem;
 `;
 
 export default function Details() {
@@ -43,6 +51,7 @@ export default function Details() {
   const [imgSrc, setImgSrc] = useState(
     "https://pbs.twimg.com/media/FI8DFOcXIAs2dnB?format=jpg&name=large"
   );
+  const [markdown, setMarkdown] = useState("");
   const { id } = useParams();
 
   const fetchActress = async () => {
@@ -52,6 +61,7 @@ export default function Details() {
     console.log(data.data);
     setActress(data.data);
     setImgSrc(data.data.images.jpg.image_url);
+    setMarkdown(marked(data.data.about));
     setLoading(false);
   };
 
@@ -74,9 +84,16 @@ export default function Details() {
               </ImgWrapper>
               <NameWrapper>
                 <Name>{actress.name}</Name>
+                <JpName>
+                  {actress.family_name}
+                  {actress.given_name}
+                </JpName>
               </NameWrapper>
             </SeiyuuHeader>
-            <p>{actress.about}</p>
+            <div>
+              <h2>Biography</h2>
+              <About dangerouslySetInnerHTML={{ __html: markdown }} />
+            </div>
           </SeiyuuWrapper>
         </Section>
       )}
