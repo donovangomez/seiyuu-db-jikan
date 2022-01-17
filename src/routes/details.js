3b5e77ee58c20f0201d2350a141e5efe9db9c2c7
Donovan <donovanjuniogomez@gmail.com>
2022-01-17 00:27:23 -0800

    add back to search in Details component

--- src/routes/details.js
+++ src/routes/details.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { marked } from "marked";
+import { Link } from "react-router-dom";
 import loadingGif from "../assets/tomori.gif";
 
 const Section = styled.section`
@@ -106,6 +107,7 @@ export default function Details() {
               <About dangerouslySetInnerHTML={{ __html: markdown }} />
             </div>
           </SeiyuuWrapper>
+          <Link to="/search">Back to Search</Link>
         </Section>
       )}
     </div>
